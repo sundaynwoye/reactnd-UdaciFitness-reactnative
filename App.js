767ea16be6a58a5859c710265116ab13d4fad054
unknown <sunnychucks1@gmail.com>
2021-11-14 18:56:02 +0100

    Common React Native Components

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <AddEntry />
+            </View>
+        )
+    }
 }
